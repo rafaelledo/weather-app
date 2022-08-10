9e526f4457222cdc5bf1a724a8ec7e45d5c33934
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-08-10 00:11:26 -0300

    ident app.js

--- app.js
+++ app.js
@@ -14,26 +14,26 @@ app.post("/", function (req, res) {
     const city = req.body.cityName
     const country = req.body.countryName
     const query = city + "," + country
-        const apiKey = "e0a5ca4b6d3e145784a463635e3cda8a"
-        const unit = "metric"
+    const apiKey = "e0a5ca4b6d3e145784a463635e3cda8a"
+    const unit = "metric"
 
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey + ""
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey + ""
 
-        https.get(url, function (response) {
-            response.on("data", function (data) {
-                const weatherData = JSON.parse(data)
-                const temperature = weatherData.main.temp
-                const weatherDescription = weatherData.weather[0].description
-                const icon = weatherData.weather[0].icon
-                const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    https.get(url, function (response) {
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            const temperature = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
-                res.write("<h1>A temperatura em "+ city + ", " + country  + " eh: " + temperature + " graus Celsius.</h1>")
-                res.write("<h2>A descricao do tempo eh: " + weatherDescription + "</h2>")
-                res.write("<img src=" + imgURL + ">")
+            res.write("<h1>A temperatura em "+ city + ", " + country  + " eh: " + temperature + " graus Celsius.</h1>")
+            res.write("<h2>A descricao do tempo eh: " + weatherDescription + "</h2>")
+            res.write("<img src=" + imgURL + ">")
 
-                res.send()
-            })
+            res.send()
         })
+    })
 })
 
 
